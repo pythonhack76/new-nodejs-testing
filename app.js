4a5109370b99b80f294cdb6b371d445235c557bd
Python The Hack <lrulvoni@gmail.com>
2022-08-20 10:16:01 +0200

    create and update file

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const http = require('http');
+const fs = require("fs");
 
 const PORT = 3004;
 
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    if (req.url === "/"){      
+    if (req.url === "/"){  
+        res.writeHead(200, { "Content-Type": "text/html"}); 
+        fs.readFile("page/home.html", "utf8", (err, data) => {
+            if (err) throw err;
+            res.write(data);
+            res.end();
+        });   
 
-    res.writeHead(200, { "Content-Type": "text/html"});
-    res.write('<h1>Welcome to our country club</h1>');
+    
+    
     //res.end();   
     }
  
-    if (req.url === '/about'){
+    else if (req.url === '/about'){
           
     res.writeHead(200, { "Content-Type": "text/html"});
-    res.write('<h1>io sono about</h1>')
-    //res.end( );  
-
+    fs.readFile("page/about.html", "utf8", (err, data) => {
+    if (err) throw err;
+    res.write(data);
+    res.end();
+    });
     }else{
         res.writeHead(404, { "Content-Type": "text/html"});
-        res.write('<h1>Pagina non trovata</h1>')
+        fs.readFile("page/404.html", "utf8", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();           
             
-            }
-            res.end();
         });
+    }
+});
+    
 
 
 console.log(`il server sta girando su ${PORT}`);

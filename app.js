6641745084b0000be5914484bc464e8e853b0ad7
Python The Hack <lrulvoni@gmail.com>
2022-08-20 09:34:43 +0200

    update error res.end

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const http = require('http');
 
-//const { area, raggio } = require("./circle");
-
-//const raggio = require("./circle");
-
-const Circle = require('./circle');
-
-const circle = new Circle();
-console.log(circle.area(9));
-console.log(circle.circumference(4));
+const PORT = 3004;
 
 
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if (req.url === "/"){      
 
-// const server = http.createServer((req, res) => {
-    // res.writeHead(200, {"Content-Type": "application/json"});
-    // res.end(
-        // JSON.stringify({
-            // data: "Salve a tutti",
-        // })
-    // );
-// });
-// 
-// 
-// server.listen(3001);
-// 
-// 
+    res.writeHead(200, { "Content-Type": "text/html"});
+    res.write('<h1>Welcome to our country club</h1>');
+    res.end();   
+    }
+ 
+    if (req.url === '/about'){
+          
+    res.writeHead(200, { "Content-Type": "text/html"});
+    res.write('<h1>io sono about</h1>')
+    res.end( );  
+
+    }else{
+        res.writeHead(404, { "Content-Type": "text/html"});
+        res.write('<h1>Pagina non trovata</h1>')
+            res.end()
+            }
+        
+        });
+
+
+console.log(`il server sta girando su ${PORT}`);
+server.listen(PORT);
